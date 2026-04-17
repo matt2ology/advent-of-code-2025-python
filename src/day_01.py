8eea13636a70fd818540bad48c7d9ce14a5359a8
matt2ology <34324060+matt2ology@users.noreply.github.com>
2026-04-16 23:42:03 -0700

    Implement initial structure for Day 01 solver with part1 and part2 methods

--- src/day_01.py
+++ src/day_01.py
@@ -1,3 +1,9 @@
 class Day01:
-    def rotary_combination_lock_password_solver(self):
-        pass
+    def __init__(self, data: list[str]):
+        self.data = data
+
+    def part1(self) -> int:
+        return len(self.data)
+
+    def part2(self) -> int:
+        return 0
