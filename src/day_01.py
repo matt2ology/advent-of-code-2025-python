b97d649ecb1f22794576f78aef23c0bdf0a3bf2f
matt2ology <34324060+matt2ology@users.noreply.github.com>
2026-04-20 00:02:05 -0700

    Fix import statements and implement part2 method in Day01 class

--- src/day_01.py
+++ src/day_01.py
@@ -1,4 +1,5 @@
-from .base_day import BaseDay
+from src.base_day import BaseDay
+from src.io import load_input_file
 
 
 class Day01(BaseDay):
@@ -8,7 +9,10 @@ class Day01(BaseDay):
     def part1(self) -> int:
         return len(self.data)
 
+    def part2(self) -> int:
+        raise NotImplementedError("Part 2 not implemented yet")
+
 
 if __name__ == "__main__":
-    data = Day01.load_input_file("day01.txt")
+    data = load_input_file("day01.txt")
     print(Day01(data).part1())
