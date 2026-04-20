5eee82583620e34867763c079155d76f915a640d
matt2ology <34324060+matt2ology@users.noreply.github.com>
2026-04-20 00:02:16 -0700

    Fix import statements and update load_input function call in main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
-from src.day_01 import Day01
-from src.io import load_input
+from .day_01 import Day01
+from .io import load_input_file
 
 
 def run_day_01():
-    data = load_input("day01.txt")
+    data = load_input_file("day01.txt")
 
     solver = Day01(data)
 
     print("Part 1:", solver.part1())
-    print("Part 2:", solver.part2())
+    # print("Part 2:", solver.part2())
 
 
 if __name__ == "__main__":
