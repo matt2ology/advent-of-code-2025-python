51a8160de3b52b7ca2e16fe3e1d512a8a5fbee51
matt2ology <34324060+matt2ology@users.noreply.github.com>
2026-04-20 00:02:38 -0700

    Fix fixture name in test_part1 method in TestDay01 class

--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -6,6 +6,6 @@ class TestDay01:
     def setup_method(self):
         self.test_file = "day01_test.txt"
 
-    def test_part1(self, load_input_file):
-        data = load_input_file(self.test_file)
+    def test_part1(self, load_input_file_fixture):
+        data = load_input_file_fixture(self.test_file)
         assert Day01(data).part1() == 3
