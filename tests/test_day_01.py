ceb4c44aac0ac00f4c317120e3a091d09575d21a
matt2ology-air <34324060+matt2ology@users.noreply.github.com>
2026-06-11 21:11:07 -0700

    Add sample input file for Day 01 and update tests to include sample case
    - Created a new input file `day01_input_sample.txt` with sample data.
    - Modified the test file `test_day_01.py` to include a test for the sample input.
    - Renamed the original test method to `test_part1_sample` to reflect the change.

--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -4,8 +4,9 @@ from src.day_01 import Day01
 class TestDay01:
 
     def setup_method(self):
-        self.test_file = "day01_test.txt"
+        self.day01_input = "day01_input.txt"
+        self.day01_input_sample = "day01_input_sample.txt"
 
-    def test_part1(self, load_input_file_fixture):
-        data = load_input_file_fixture(self.test_file)
+    def test_part1_sample(self, load_input_file_fixture):
+        data = load_input_file_fixture(self.day01_input_sample)
         assert Day01(data).part1() == 3
