808644427fc49adb78e03fcb8e8f79d415087c37
matt2ology <34324060+matt2ology@users.noreply.github.com>
2026-06-12 20:39:46 -0700

    Refactoring

--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -4,9 +4,8 @@ from src.day_01 import Day01
 class TestDay01:
 
     def setup_method(self):
-        self.day01_input = "day01_input.txt"
-        self.day01_input_sample = "day01_input_sample.txt"
+        self.day01_input_sample_3_zero = "day01_input_sample_3_zero.txt"
 
-    def test_part1_sample(self, load_input_file_fixture):
-        data = load_input_file_fixture(self.day01_input_sample)
+    def test_part1_sample_3_zero(self, load_input_file_fixture):
+        data = load_input_file_fixture(self.day01_input_sample_3_zero)
         assert Day01(data).part1() == 3
